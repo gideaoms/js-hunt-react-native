09f07a7fadea03be91553666b28839047c884496
Gideão Silva <gideaoms@gmail.com>
2018-12-20 18:10:25 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# js-hunt-react-native
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Main from "./pages/main";
+import Product from "./pages/product";
+
+const AppNavigator = createStackNavigator(
+  {
+    Main,
+    Product
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#DA552F"
+      },
+      headerTintColor: "#FFF"
+    }
+  }
+);
+
+export default createAppContainer(AppNavigator);
